2185e3a37f13385f0e4e7fb85a094a75fc5fc545
luca-go <54832122+luca-go@users.noreply.github.com>
2022-04-28 12:41:34 +0200

    providing masks now actually works

--- src/image_processing.py
+++ src/image_processing.py
@@ -4,12 +4,14 @@ from PIL import Image
 import io
 
 def make_mask(image):
+    print(type(image))
     if type(image) == bytes:
+
         wine_mask = np.array(Image.open(io.BytesIO(image)))
+        wine_mask[wine_mask == 0] = 255
+
     else:
         wine_mask = np.array(Image.open(image))
-
-    wine_mask[wine_mask == 0] = 255
 
     return wine_mask
 
@@ -24,7 +26,7 @@ def black_and_white(img_path):
     bw_img = img.convert('L').point(fn, mode='1')
 
     with io.BytesIO() as output:
-        bw_img.save(output, format="PNG")
+        bw_img.save(output, format="png")
         bw_img_io = output.getvalue()
 
     return bw_img_io
