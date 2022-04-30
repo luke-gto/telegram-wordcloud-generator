1d344992fdf483c3206a58a5df9847b4ba68fe39
luca-go <54832122+luca-go@users.noreply.github.com>
2022-04-30 12:24:19 +0200

    ui added in main.py

--- main.py
+++ main.py
@@ -1,51 +1,47 @@
-import easygui
-import os
+from ui.main_window import Ui_MainWindow
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QMessageBox
+from PyQt5.uic import loadUi
+from PyQt5.QtCore import pyqtSlot
+from src.wordcloud import tokenize_chat
+
 import sys
 
-from src.wordcloud import load_json, tokenize_chat, map_wordcloud, standard_wordcloud
 
+class Window(QMainWindow, Ui_MainWindow):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+        self.setupUi(self)
+        self.choose_json()
+        self.choose_stopwords()
+    #     self.select_option()
 
-script_directory = os.path.dirname(os.path.realpath(__file__))
+    # def select_option(self):
+    #     self.buttonGroup.IdChecked()
 
+    def choose_json(self):
+        self.pushButton_json.clicked.connect(self.browse_file_json)
 
-def main():
+    def choose_stopwords(self):
+        self.pushButton_stopwords.clicked.connect(self.browse_file_stopwords)
 
-    choice = input(
-        "\nWhat type of wordcloud do you want to generate?\n1. Standard\n2. wordcloud with image map\n3. I want only the chat messages without stopwords ready for some text mining, you incompetent!\n\n"
-    )
-
-    chat = tokenize_chat(load_json())  # input chat json file
-
-    chat_as_text = " ".join(chat)  # from list to string --> needed by wordcloud library
-
-    if choice == "1":
-        standard_wordcloud(chat_as_text)
-        sys.exit()
-
-    if choice == "2":
-        map_wordcloud(chat_as_text)
-        sys.exit()
-
-    if choice == "3":
-        print("Loading...\n")
-        easygui.msgbox(
-            "Select the folder where you want to save the chat messages ready for some text mining with third party tools",
-            "Telegram chat wordcloud generator",
+    def browse_file_stopwords(self):
+        stopwords_path = QtWidgets.QFileDialog.getOpenFileName(
+            self, "Open File", "", "*.txt"
         )
-        save_directory = easygui.diropenbox(
-            msg="Select the folder", default=script_directory
+        self.label_stopwords.setText("Selected file: " + stopwords_path[0])
+        return stopwords_path
+
+    def browse_file_json(self):
+        json_path = QtWidgets.QFileDialog.getOpenFileName(
+            self, "Open File", "", "*.json"
         )
-
-        with open(save_directory + "/telegram_chat.txt", "w") as f:
-            f.write(chat_as_text)
-        print("Saving the exported chat in " + script_directory)
-        sys.exit()
-
-    else:
-        print("\n\nPlease choose a valid option: 1, 2 or 3")
-        sys.exit()
+        self.label_json.setText("Selected file: " + json_path[0])
+        return json_path
 
 
 if __name__ == "__main__":
-
-    main()
+    app = QApplication(sys.argv)
+    win = Window()
+    win.show()
+    sys.exit(app.exec())
