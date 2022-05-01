5e9c0867e152d3c4e2e32421cfebd9eb7e26d515
luca-go <54832122+luca-go@users.noreply.github.com>
2022-05-01 16:25:06 +0200

    color pickers added

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 from ui.main_window import Ui_MainWindow
 from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QApplication, QDialog, QMainWindow, QMessageBox
+from PyQt5.QtWidgets import (
+    QApplication,
+    QDialog,
+    QMainWindow,
+    QMessageBox,
+    QProgressDialog,
+    QProgressBar,
+    QColorDialog,
+)
 import src.wordcloud as wc
 import os
 from pathlib import Path
-
+import threading
 import sys
 
 script_directory = os.path.dirname(os.path.realpath(__file__))
@@ -16,12 +24,31 @@ class Window(QMainWindow, Ui_MainWindow):
         super().__init__(parent)
         self.setupUi(self)
         self.buttons_listener()
+        self.progressBar.hide()
 
     def buttons_listener(self):
+
         self.pushButton_start.clicked.connect(self.chosen_option)
         self.pushButton_json.clicked.connect(self.browse_file_json)
         self.pushButton_stopwords.clicked.connect(self.browse_file_stopwords)
         self.pushButton_font.clicked.connect(self.browse_file_font)
+        self.pushButton_back_color.clicked.connect(self.back_color_picker)
+        self.pushButton_cont_color.clicked.connect(self.cont_color_picker)
+
+    def back_color_picker(self):
+        color = QColorDialog.getColor()
+        if color.isValid():
+            global back_color_code
+            back_color_code = color.name()
+            self.pushButton_back_color.setStyleSheet("background-color : {}".format(back_color_code))
+
+    def cont_color_picker(self):
+        color = QColorDialog.getColor()
+        if color.isValid():
+            global cont_color_code
+            cont_color_code = color.name()
+            self.pushButton_cont_color.setStyleSheet("background-color : {}".format(cont_color_code))
+
 
     def browse_file_font(self):
 
@@ -82,16 +109,18 @@ class Window(QMainWindow, Ui_MainWindow):
 
         scale = self.doubleSpinBox_scale.value()
 
-        if self.lineEdit_back_color.text() == "":
-            background_color = None
-        else:
-            background_color = self.lineEdit_back_color.text()
+        background_color = back_color_code
 
         color_mode = self.lineEdit_color_mode.text()
         prefer_horizontal = self.doubleSpinBox_horizontal.value()
         collocation_threshold = self.spinBox_collocation.value()
         relative_scaling = self.doubleSpinBox_relative_scale.value()
-        mask_contour_color = self.lineEdit_cont_color.text()
+
+        if "cont_color_code" not in locals():
+            mask_contour_color = None
+        else:
+            mask_contour_color = cont_color_code
+
         contour_width = self.spinBox_contour.value()
 
         return (
@@ -102,11 +131,11 @@ class Window(QMainWindow, Ui_MainWindow):
             font,
             min_font_size,
             font_step,
-            max_font,  
-            max_words,  
-            min_word_lenght,  
-            scale,  
-            background_color,  
+            max_font,
+            max_words,
+            min_word_lenght,
+            scale,
+            background_color,
             color_mode,
             prefer_horizontal,
             collocation_threshold,
@@ -117,6 +146,9 @@ class Window(QMainWindow, Ui_MainWindow):
 
     def chosen_option(self):
 
+        self.progressBar.show()
+        self.pushButton_start.setCursor(QtGui.QCursor(QtCore.Qt.WaitCursor))
+
         tokenized_chat_output = wc.tokenize_chat(
             self.output_jdata(json_path[0]), stopwords_path[0]
         )
@@ -125,6 +157,7 @@ class Window(QMainWindow, Ui_MainWindow):
         save_path = tokenized_chat_output[1]
 
         if self.buttonGroup.checkedId() == -2:  ### text only no wordcloud
+
             with open(save_path + "/telegram_chat.txt", "w") as f:
                 f.write(chat_as_text)
                 msg = QMessageBox()
@@ -136,11 +169,13 @@ class Window(QMainWindow, Ui_MainWindow):
                 )
                 msg.setIcon(msg.Information)
                 msg.exec()
+                self.progressBar.hide()
+                self.pushButton_start.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+
                 return
 
         if self.buttonGroup.checkedId() == -4:
             options = self.read_wordcloud_options()
-            print(options)
             wc.standard_wordcloud(
                 save_path=save_path,
                 tokenized_chat=chat_as_text,
@@ -161,6 +196,13 @@ class Window(QMainWindow, Ui_MainWindow):
                 min_word_length=options[9],
                 collocation_threshold=options[14],
             )
+            self.progressBar.hide()
+            self.pushButton_start.setCursor(QtGui.QCursor(QtCore.Qt.ArrowCursor))
+
+        if self.buttonGroup.checkedId() == -3:
+            options = self.read_wordcloud_options()
+
+            pass
 
 
 if __name__ == "__main__":
